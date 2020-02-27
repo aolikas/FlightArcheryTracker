511bc87e6820b69eddf52729a3593fa5d90f8113
aolikas <olkaspark@gmail.com>
2020-02-27 15:02:17 +0100

    added room repository and viewModel

---
+++ app/src/main/java/com/example/flightarcherytracker/viewModel/ShootViewModel.java
@@ -0,0 +1,53 @@
+package com.example.flightarcherytracker.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.flightarcherytracker.entity.Shoot;
+import com.example.flightarcherytracker.repository.ShootRepository;
+
+import java.util.List;
+
+
+public class ShootViewModel extends AndroidViewModel {
+
+    private ShootRepository mShootRepository;
+    private LiveData<List<Shoot>> mAllShoots;
+
+    public ShootViewModel(@NonNull Application application) {
+        super(application);
+        mShootRepository = new ShootRepository(application);
+        mAllShoots = mShootRepository.getAllShoots();
+    }
+
+    public void insertShoot(Shoot shoot) {
+        mShootRepository.insertShoot(shoot);
+    }
+
+    public void updateShoot(Shoot shoot) {
+        mShootRepository.updateShoot(shoot);
+    }
+
+    public void deleteShoot(Shoot shoot) {
+        mShootRepository.deleteShoot(shoot);
+    }
+
+    public void deleteAllShoots() {
+        mShootRepository.deleteAllShoots();
+    }
+
+    public LiveData<List<Shoot>> getAllShoots() {
+        return mAllShoots;
+    }
+
+    public LiveData<List<Shoot>> getAllShootsByTrainingId(long trainingId) {
+        return mShootRepository.getAllShootsByTrainingId(trainingId);
+    }
+
+    public LiveData<List<Shoot>> getAllShootsLatLngByTrainingId(long trainingId) {
+        return mShootRepository.getAllShootLatLngByTrainingId(trainingId);
+    }
+}
---
+++ app/src/main/java/com/example/flightarcherytracker/viewModel/TrainingViewModel.java
@@ -0,0 +1,52 @@
+package com.example.flightarcherytracker.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.flightarcherytracker.entity.Training;
+import com.example.flightarcherytracker.repository.TrainingRepository;
+
+import java.util.List;
+
+public class TrainingViewModel extends AndroidViewModel {
+
+    private TrainingRepository mTrainingRepository;
+    private LiveData<List<Training>> mAllTrainings;
+
+    public TrainingViewModel(@NonNull Application application) {
+        super(application);
+        mTrainingRepository = new TrainingRepository(application);
+        mAllTrainings = mTrainingRepository.getAllTrainings();
+    }
+
+    public void insertTraining(Training training) {
+        mTrainingRepository.insertTraining(training);
+    }
+
+    public void updateTraining(Training training) {
+        mTrainingRepository.updateTraining(training);
+    }
+
+    public long insertTrainingWithId(Training training) {
+        return mTrainingRepository.insertTrainingWithId(training);
+    }
+
+    public void deleteTraining(Training training) {
+        mTrainingRepository.deleteTraining(training);
+    }
+
+    public void deleteAllTrainings() {
+        mTrainingRepository.deleteAllTrainings();
+    }
+
+    public LiveData<List<Training>> getAllTrainings() {
+        return mAllTrainings;
+    }
+
+    public long getTrainingById(long id) {
+        return mTrainingRepository.getTrainingById(id);
+    }
+}
